3f8b6df4fc38f4a1c7cc04685b95d32b20cac933
Gajus Kuizinas <gajus@gajus.com>
2022-03-17 10:43:50 -0600

    test: ensure that we are serving br builds

--- test/fastify-webpack-hmr/fastifyWebpack.ts
+++ test/fastify-webpack-hmr/fastifyWebpack.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import test from 'ava';
+import CompressionPlugin from 'compression-webpack-plugin';
 import createFastify from 'fastify';
 import got from 'got';
 import webpack from 'webpack';
@@ -30,4 +31,41 @@ test('builds and serves bundle', async (t) => {
   });
 
   t.is(response, 'console.log("Hello, World");');
+});
+
+test('serves brotli compressed assets when available', async (t) => {
+  const app = createFastify();
+
+  const compiler = webpack({
+    entry: path.resolve(__dirname, '../fixtures/index.js'),
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, '/dist'),
+      publicPath: '/',
+    },
+    plugins: [
+      new CompressionPlugin({
+        algorithm: 'brotliCompress',
+        deleteOriginalAssets: false,
+        filename: '[path][base].br',
+        minRatio: Number.POSITIVE_INFINITY,
+        test: /\.js$/u,
+        threshold: 0,
+      }),
+    ],
+  });
+
+  void app.register(fastifyWebpack, {
+    compiler,
+  });
+
+  const serverAddress = await app.listen(0);
+
+  const response = await got(serverAddress + '/main.js', {
+    headers: {
+      'accept-encoding': 'br',
+    },
+  });
+
+  t.is(response.headers['content-encoding'], 'br');
 });
